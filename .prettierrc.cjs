ea79708d73dde79001c29b73a001b3b806b38e75
Denis Omar Cuyo Ttito <182906@unsaac.edu.pe>
2023-06-09 17:29:35 -0500

    Initial commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  quoteProps: "as-needed",
+  singleQuote: false,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: "all",
+};
