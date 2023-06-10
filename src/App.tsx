e2e1e904e91ea4172bbfbcb1c08910fe826dfa7e
Denis Omar Cuyo Ttito <182906@unsaac.edu.pe>
2023-06-09 19:29:12 -0500

    set up theme and global styles
    - Fonts families, colors
    - Setup alias to src dirs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,104 @@
+import { Global, MantineProvider } from "@mantine/core";
 import Home from "@pages/Home";
+import fraunces from "./fonts/Fraunces_144pt-SemiBold.ttf";
+import manrope from "./fonts/Manrope-Regular.ttf";
+
+const GlobalStyles = () => {
+  return (
+    <Global
+      styles={[
+        {
+          "@font-face": {
+            fontFamily: "Fraunces",
+            src: `url("${fraunces}") format("truetype")`,
+            fontWeight: 600,
+            fontStyle: "normal",
+          },
+        },
+        {
+          "@font-face": {
+            fontFamily: "Manrope",
+            src: `url("${manrope}") format('truetype')`,
+            fontWeight: 400,
+            fontStyle: "normal",
+          },
+        },
+      ]}
+    />
+  );
+};
 
 const App = () => {
-  return <Home />;
+  return (
+    <MantineProvider
+      theme={{
+        colors: {
+          _yourDarkness: [
+            "#E9E6EC",
+            "#D3CDD8",
+            "#BDB4C5",
+            "#A79BB2",
+            "#7C698B",
+            "#503765",
+            "#24053E",
+            "#1D0432",
+            "#160325",
+            "#0E0219",
+          ],
+          _seaGreen: [
+            "#ECFFF6",
+            "#DAFFEC",
+            "#C7FFE3",
+            "#B4FFD9",
+            "#8FFFC7",
+            "#69FFB4",
+            "#44FFA1",
+            "#36CC81",
+            "#299961",
+            "#1B6640",
+          ],
+          _illusionist: [
+            "#EEEDEF",
+            "#DEDBE0",
+            "#CDCAD0",
+            "#BCB8C0",
+            "#9B94A1",
+            "#797181",
+            "#584D62",
+            "#463E4E",
+            "#352E3B",
+            "#231F27",
+          ],
+          _coldSnow: [
+            "#FFFEFF",
+            "#FEFEFF",
+            "#FEFDFF",
+            "#FEFCFF",
+            "#FDFBFF",
+            "#FDF9FF",
+            "#FCF8FF",
+            "#979599",
+            "#656366",
+            "#4C4A4D",
+          ],
+        },
+        headings: {
+          fontFamily: "Fraunces, sans-serif",
+          sizes: {
+            h1: {
+              fontSize: "50px",
+            },
+          },
+        },
+        fontFamily: "Manrope, sans-serif",
+      }}
+      withGlobalStyles
+      withNormalizeCSS
+    >
+      <GlobalStyles />
+      <Home />
+    </MantineProvider>
+  );
 };
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,30 @@
+import { Button, Title } from "@mantine/core";
+
 const Home = () => {
-  return <>Home</>;
+  return (
+    <>
+      <Title color="_yourDarkness">Data tailored to your needs.</Title>
+      <Button
+        bg="_seaGreen"
+        size="lg"
+        styles={theme => ({
+          label: {
+            color: theme.colors._yourDarkness[6],
+            "&:hover": {
+              color: theme.colors._seaGreen[6],
+            },
+          },
+          root: {
+            "&:hover": {
+              backgroundColor: theme.colors._yourDarkness[6],
+            },
+          },
+        })}
+      >
+        Apply your access
+      </Button>
+    </>
+  );
 };
 
 export default Home;
